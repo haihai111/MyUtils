861ac0c857b0f21a34979da53fdd361b4fefa69e
haipv <haipv@sendo.vn>
2018-06-14 15:36:00 +0700

    add react native

--- android/myutils/src/main/java/com/app/myutils/Math.java
+++ android/myutils/src/main/java/com/app/myutils/Math.java
@@ -1,5 +1,8 @@
 package com.app.myutils;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
 public class Math {
 
     public static int Plus(int a, int b) {
@@ -9,5 +12,4 @@ public class Math {
     public static int Minus(int a, int b) {
         return a - b;
     }
-
 }
